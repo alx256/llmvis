5b8c614a56d6af1686e9aa96658846ff60b74293
Alexander Poulson <alexanderipoulson@gmail.com>
2024-12-10 20:15:16 +0000

    feat: add package building and installation support

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'llmvis',
+    version = '0.1',
+    author = 'Alex Poulson',
+    author_email = 'psya14@nottingham.ac.uk',
+    description = 'Library for XAI visualizations of LLM applications',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    packages = ['llmvis'] + ['llmvis.' + pkg for pkg in find_packages('llmvis')],
+    package_data = {
+        'llmvis.visualization': ["css/*.css", "html/*.html", "js/*.js"]
+    }
+)
