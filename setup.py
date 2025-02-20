b0adeb113655426b13ea1877092b82a31834d280
Alexander Poulson <alexanderipoulson@gmail.com>
2025-02-20 00:37:26 +0000

    feat: add initial support for watsonx.ai

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'llmvis',
-    version = '0.1',
-    author = 'Alex Poulson',
-    author_email = 'psya14@nottingham.ac.uk',
-    description = 'Library for XAI visualizations of LLM applications',
-    long_description = open('README.md').read(),
-    long_description_content_type = 'text/markdown',
-    packages = ['llmvis'] + ['llmvis.' + pkg for pkg in find_packages('llmvis')],
-    package_data = {
-        'llmvis' : ['assets/fonts/*'],
-        'llmvis.visualization': ['css/*.css', 'html/*.html', 'js/*.js']
+    name="llmvis",
+    version="0.1",
+    author="Alex Poulson",
+    author_email="psya14@nottingham.ac.uk",
+    description="Library for XAI visualizations of LLM applications",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=["llmvis"] + ["llmvis." + pkg for pkg in find_packages("llmvis")],
+    package_data={
+        "llmvis": ["assets/fonts/*"],
+        "llmvis.visualization": ["css/*.css", "html/*.html", "js/*.js"],
     },
-    install_requires = [
-        'ollama',
-        'numpy',
-        'scikit-learn',
-        'IPython',
-        'PyQt6',
-        'PyQt6-WebEngine'
-    ]
+    install_requires=[
+        "ollama",
+        "numpy",
+        "scikit-learn",
+        "IPython",
+        "PyQt6",
+        "PyQt6-WebEngine",
+        "requests",
+    ],
 )
