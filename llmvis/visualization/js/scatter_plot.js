248277f58be0492f5eed20db68233bfcd4e06f97
Alexander Poulson <alexanderipoulson@gmail.com>
2025-03-25 17:48:43 +0000

    feat: add nice scaling to scatter plots

--- llmvis/visualization/js/scatter_plot.js
+++ llmvis/visualization/js/scatter_plot.js
@@ -13,84 +13,36 @@ function drawScatterPlot(canvasId, scatterPlotPlots) {
     const SCATTER_PLOT_CTX = SCATTER_PLOT_CANVAS.getContext('2d');
 
     const SCATTER_PLOT_STROKE_COLOR = 'rgb(222, 222, 222)';
-    const SCATTER_PLOT_PLOT_RADIUS = 3;
-    const SCATTER_PLOT_AXIS_PADDING = 17;
+    const SCATTER_PLOT_PLOT_RADIUS = 7;
+    const SCATTER_PLOT_AXIS_PADDING = 44;
 
     // Find maximum x and y values
     var maxX = -1;
     var maxY = -1;
+    var minX = -1;
+    var minY = -1;
 
     for (plot of scatterPlotPlots) {
-        const X = Math.abs(plot[0]);
-        const Y = Math.abs(plot[1]);
+        const X = plot[0];
+        const Y = plot[1];
 
-        if (X > maxX) {
+        if (maxX == -1 || X > maxX) {
             maxX = X;
         }
 
-        if (Y > maxY) {
+        if (maxY == -1 || Y > maxY) {
             maxY = Y;
+        }
+
+        if (minX == -1 || X < minX) {
+            minX = X;
+        }
+
+        if (minY == -1 || Y < minY) {
+            minY = Y;
         }
     }
 
-    // Multiply by 2 to account for both
-    // positive and negative values
-    maxX *= 2;
-    maxY *= 2;
-
-    drawAxes(SCATTER_PLOT_CANVAS, SCATTER_PLOT_CTX, SCATTER_PLOT_STROKE_COLOR,
-        maxX, maxY, SCATTER_PLOT_AXIS_PADDING);
-
-    SCATTER_PLOT_CTX.strokeStyle = SCATTER_PLOT_STROKE_COLOR;
-    SCATTER_PLOT_CTX.beginPath();
-    for (plot of scatterPlotPlots) {
-        const TRANSFORMED = graphToScreen(SCATTER_PLOT_CANVAS, SCATTER_PLOT_AXIS_PADDING,
-            plot[0], plot[1], maxX, maxY);
-
-        SCATTER_PLOT_CTX.moveTo(TRANSFORMED.x + SCATTER_PLOT_PLOT_RADIUS, TRANSFORMED.y);
-        SCATTER_PLOT_CTX.arc(TRANSFORMED.x, TRANSFORMED.y,
-            SCATTER_PLOT_PLOT_RADIUS, 0, 2 * Math.PI);
-    }
-
-    SCATTER_PLOT_CTX.stroke();
-
-    enableResizing(SCATTER_PLOT_CANVAS, function() {
-        drawScatterPlot(canvasId, scatterPlotPlots);
-    });
-}
-
-function conciseString(num) {
-    const SCATTER_PLOT_MAXIMUM_NUMBER_LENGTH = 4;
-    const SCATTER_PLOT_EXPONENT_FRACTION_DIGITS = 2;
-    const s = num.toString();
-
-    if (num.toString().length <= SCATTER_PLOT_MAXIMUM_NUMBER_LENGTH) {
-        return s;
-    }
-
-    return num.toExponential(SCATTER_PLOT_EXPONENT_FRACTION_DIGITS);
-}
-
-/**
- * Draw a set of axes that stretch from -maxX to maxX on the
- * x-axis and -maxY to maxY on the y-axis.
- * 
- * @param {Object} canvas The canvas that these axes should be
- *      drawn to.
- * @param {Object} ctx The context that should be used for
- *      drawing.
- * @param {string} color The color that should be used for
- *      drawing these axes.
- * @param {number} maxX The maximum x position out of all the points
- *      that need to be plotted. This will be used to determine how
- *      many points should be shown on the x-axis.
- * @param {number} maxY The maximum y position out of all the points
- *      that need to be plotted. This will be used to determine how
- *      many points should be shown on the y-axis.
- * @param {number} padding The padding between the end of the canvas
- *      and the axes.
- */
-function drawAxes(canvas, ctx, color, maxX, maxY, padding) {
     const SCATTER_PLOT_STEP_COUNT = 20;
     const SCATTER_PLOT_MARKING_LENGTH = 5;
     const SCATTER_PLOT_MARKINGS_MATRIX = [
@@ -99,65 +51,110 @@ function drawAxes(canvas, ctx, color, maxX, maxY, padding) {
         [0, 1],
         [0, -1]
     ];
-    const AXIS_START_POINT_X = padding;
-    const AXIS_START_POINT_Y = canvas.height - padding;
-    const AXIS_END_POINT_X = canvas.width - padding;
-    const AXIS_END_POINT_Y = padding;
-    const SCATTER_PLOT_X_AXIS_Y = canvas.height / 2;
-    const SCATTER_PLOT_Y_AXIS_X = canvas.width / 2;
+    const AXIS_START_POINT_X = SCATTER_PLOT_AXIS_PADDING;
+    const AXIS_START_POINT_Y = SCATTER_PLOT_CANVAS.height - SCATTER_PLOT_AXIS_PADDING;
+    const AXIS_END_POINT_X = SCATTER_PLOT_CANVAS.width - SCATTER_PLOT_AXIS_PADDING;
+    const AXIS_END_POINT_Y = SCATTER_PLOT_AXIS_PADDING;
+    const SCATTER_PLOT_X_AXIS_Y = AXIS_START_POINT_Y;
+    const SCATTER_PLOT_Y_AXIS_X = AXIS_START_POINT_X;
 
-    ctx.strokeStyle = color;
-    ctx.beginPath();
+    SCATTER_PLOT_CTX.strokeStyle = SCATTER_PLOT_STROKE_COLOR;
+    SCATTER_PLOT_CTX.beginPath();
 
     // X Axis
-    ctx.moveTo(AXIS_START_POINT_X, SCATTER_PLOT_X_AXIS_Y);
-    ctx.lineTo(AXIS_END_POINT_X, SCATTER_PLOT_X_AXIS_Y);
+    SCATTER_PLOT_CTX.moveTo(AXIS_START_POINT_X, SCATTER_PLOT_X_AXIS_Y);
+    SCATTER_PLOT_CTX.lineTo(AXIS_END_POINT_X, SCATTER_PLOT_X_AXIS_Y);
 
     // Y Axis
-    ctx.moveTo(SCATTER_PLOT_Y_AXIS_X, AXIS_START_POINT_Y);
-    ctx.lineTo(SCATTER_PLOT_Y_AXIS_X, AXIS_END_POINT_Y);
-    
-    var stepX = (canvas.width - padding*2) / SCATTER_PLOT_STEP_COUNT;
-    var stepY = (canvas.height - padding*2) / SCATTER_PLOT_STEP_COUNT;
+    SCATTER_PLOT_CTX.moveTo(SCATTER_PLOT_Y_AXIS_X, AXIS_START_POINT_Y);
+    SCATTER_PLOT_CTX.lineTo(SCATTER_PLOT_Y_AXIS_X, AXIS_END_POINT_Y);
+    SCATTER_PLOT_CTX.stroke();
 
-    for (var i = 0; i < SCATTER_PLOT_STEP_COUNT; i++) {
-        const X = i * stepX;
-        const Y = i * stepY;
+    var stepX = niceStep(maxX - minX, SCATTER_PLOT_STEP_COUNT);
+    var stepY = niceStep(maxY - minY, SCATTER_PLOT_STEP_COUNT);
 
-        ctx.fontStyle = "5px DidactGothic";
-        ctx.fillStyle = color;
+    // Set the minimum to the closest step equal to
+    // or below it.
+    minX = stepX*Math.floor(minX/stepX);
+    minY = stepY*Math.floor(minY/stepY);
 
-        // Draw markings
-        for (const multipliers of SCATTER_PLOT_MARKINGS_MATRIX) {
-            const coords = screenToGraph(canvas, padding, multipliers[0], multipliers[1], maxX, maxY);
-            const coordsStr = conciseString((multipliers[0] > 0) ? coords.x : coords.y);
-            const measurements = ctx.measureText(coordsStr);
-            const startX = SCATTER_PLOT_Y_AXIS_X + X*multipliers[0];
-            const startY = SCATTER_PLOT_X_AXIS_Y + Y*multipliers[1];
-            const isX = multipliers[0] != 0;
+    // Adjust maximum to be SCATTER_PLOT_STEP_COUNT
+    // steps away from the minimum, with an additional
+    // step for safety.
+    maxX = minX + stepX*(SCATTER_PLOT_STEP_COUNT+1);
+    maxY = minY + stepY*(SCATTER_PLOT_STEP_COUNT+1);
 
-            ctx.moveTo(startX, startY);
-            ctx.lineTo(startX + (isX ? 0 : SCATTER_PLOT_MARKING_LENGTH),
-                startY + (isX ? SCATTER_PLOT_MARKING_LENGTH : 0));
+    var xTickPos = minX;
+    var yTickPos = minY;
 
-            ctx.save();
-            ctx.translate(
-                startX + (isX ? 0 : SCATTER_PLOT_MARKING_LENGTH +
-                    measurements.actualBoundingBoxAscent + measurements.actualBoundingBoxDescent),
-                startY + (isX ? SCATTER_PLOT_MARKING_LENGTH +
-                    measurements.actualBoundingBoxAscent + measurements.actualBoundingBoxDescent : 0)
-            );
+    SCATTER_PLOT_CTX.fontStyle = "15px DidactGothic";
+    SCATTER_PLOT_CTX.fillStyle = SCATTER_PLOT_STROKE_COLOR;
 
-            if (isX) {
-                ctx.rotate(Math.PI / 2)
-            }
+    while (xTickPos <= maxX || yTickPos <= maxY) {
+        // Fix potential floating point problems
+        xTickPos = parseFloat(xTickPos.toPrecision(12));
+        yTickPos = parseFloat(yTickPos.toPrecision(12));
 
-            ctx.fillText(coordsStr, 0, 0);
-            ctx.restore();
+        const SCREEN_COORDS = graphToScreen(SCATTER_PLOT_CANVAS, SCATTER_PLOT_AXIS_PADDING,
+            xTickPos, yTickPos, minX, minY, maxX, maxY);
+        const MEASUREMENTS_X = SCATTER_PLOT_CTX.measureText(xTickPos.toString());
+        const MEASUREMENTS_Y = SCATTER_PLOT_CTX.measureText(yTickPos.toString());
+        const TEXT_WIDTH_X = MEASUREMENTS_X.width;
+        const TEXT_HEIGHT_X = MEASUREMENTS_X.actualBoundingBoxAscent +
+            MEASUREMENTS_X.actualBoundingBoxDescent;
+        const TEXT_WIDTH_Y = MEASUREMENTS_Y.width;
+        const TEXT_HEIGHT_Y = MEASUREMENTS_Y.actualBoundingBoxAscent +
+            MEASUREMENTS_Y.actualBoundingBoxDescent;
+
+        SCATTER_PLOT_CTX.beginPath();
+
+        // X Axis
+        if (xTickPos <= maxX) {
+            SCATTER_PLOT_CTX.moveTo(SCREEN_COORDS.x, SCATTER_PLOT_X_AXIS_Y);
+            SCATTER_PLOT_CTX.lineTo(SCREEN_COORDS.x, SCATTER_PLOT_X_AXIS_Y + SCATTER_PLOT_MARKING_LENGTH);
+
+            SCATTER_PLOT_CTX.fillText(xTickPos.toString(),
+                SCREEN_COORDS.x - TEXT_WIDTH_X/2,
+                SCATTER_PLOT_X_AXIS_Y + SCATTER_PLOT_MARKING_LENGTH + TEXT_HEIGHT_X);
         }
+
+        // Y Axis
+        if (yTickPos <= maxY) {
+            SCATTER_PLOT_CTX.moveTo(SCATTER_PLOT_Y_AXIS_X, SCREEN_COORDS.y);
+            SCATTER_PLOT_CTX.lineTo(SCATTER_PLOT_Y_AXIS_X - SCATTER_PLOT_MARKING_LENGTH, SCREEN_COORDS.y);
+
+            SCATTER_PLOT_CTX.fillText(yTickPos.toString(),
+                SCATTER_PLOT_Y_AXIS_X - SCATTER_PLOT_MARKING_LENGTH*2 - TEXT_WIDTH_Y,
+                SCREEN_COORDS.y + TEXT_HEIGHT_Y/2);
+        }
+
+        xTickPos += stepX;
+        yTickPos += stepY;
+        SCATTER_PLOT_CTX.stroke();
     }
 
-    ctx.stroke();
+    // Draw points
+    SCATTER_PLOT_CTX.strokeStyle = SCATTER_PLOT_STROKE_COLOR;
+    SCATTER_PLOT_CTX.beginPath();
+    for (var i = 0; i < scatterPlotPlots.length; i++) {
+        const PLOT = scatterPlotPlots[i];
+        const TRANSFORMED = graphToScreen(SCATTER_PLOT_CANVAS, SCATTER_PLOT_AXIS_PADDING,
+            PLOT[0], PLOT[1], minX, minY, maxX, maxY);
+        const RGB = calculateRgb(1, 2, 0);
+
+        SCATTER_PLOT_CTX.beginPath();
+        SCATTER_PLOT_CTX.strokeStyle = RGB;
+        SCATTER_PLOT_CTX.fillStyle = RGB;
+        SCATTER_PLOT_CTX.arc(TRANSFORMED.x,
+            TRANSFORMED.y,
+            SCATTER_PLOT_PLOT_RADIUS, 0, 2 * Math.PI);
+        SCATTER_PLOT_CTX.fill();
+        SCATTER_PLOT_CTX.stroke();
+    }
+
+    enableResizing(SCATTER_PLOT_CANVAS, function() {
+        drawScatterPlot(canvasId, scatterPlotPlots);
+    });
 }
 
 /**
@@ -171,6 +168,10 @@ function drawAxes(canvas, ctx, color, maxX, maxY, padding) {
  *      and the axes.
  * @param {number} x The x position of the point in "graph space".
  * @param {number} y The y position of the point in "graph space".
+ * @param {number} minX The minimum x position of any point that needs
+ *      to be plotted.
+ * @param {number} minY The maximum y position of any point that needs
+ *      to be plotted.
  * @param {number} maxX The maximum x position of any point that needs
  *      to be plotted.
  * @param {number} maxY The maximum y position of any point that needs
@@ -179,19 +180,15 @@ function drawAxes(canvas, ctx, color, maxX, maxY, padding) {
  * "screen space" and y is the y position of the point in
  * "screen space".
  */
-function graphToScreen(canvas, padding, x, y, maxX, maxY) {
+function graphToScreen(canvas, padding, x, y, minX, minY, maxX, maxY,) {
     // Scale such that it can fit on the graph
-    x = (x / maxX) * (canvas.width - padding*2);
-    y = (y / maxY) * (canvas.height - padding*2);
-
-    // Shift such that (0, 0) -> (width / 2, height / 2)
-    x += canvas.width / 2;
-    y += canvas.height / 2;
+    x = ((x - minX) / (maxX - minX)) * (canvas.width - padding*2);
+    y = ((y - minY) / (maxY - minY)) * (canvas.height - padding*2);
 
     // Invert y so that it is in the proper place in the canvas
     y = canvas.height - y;
 
-    return {x: x, y: y};
+    return {x: x + padding, y: y - padding};
 }
 
 /**
@@ -205,6 +202,10 @@ function graphToScreen(canvas, padding, x, y, maxX, maxY) {
  *      and the axes.
  * @param {number} x The x position of the point in "screen space".
  * @param {number} y The y position of the point in "screen space".
+ * @param {number} minX The minimum x position of any point that needs to be
+ *      plotted.
+ * @param {number} minY The minimum y position of any point that needs to be
+ *      plotted.
  * @param {number} maxX The maximum x position of any point that needs to be
  *      plotted.
  * @param {number} maxY The maximum y position of any point that needs to be
@@ -213,20 +214,19 @@ function graphToScreen(canvas, padding, x, y, maxX, maxY) {
  * "graph space" and y is the y position of the point in
  * "graph space".
  */
-function screenToGraph(canvas, padding, x, y, maxX, maxY) {
+function screenToGraph(canvas, padding, x, y, minX, minY, maxX, maxY) {
+    x -= padding;
+    y += padding;
+
     // Inverse invert
     y -= canvas.height;
     y *= -1;
 
-    // Inverse shift
-    x -= canvas.width / 2;
-    y -= canvas.height / 2;
-
     // Inverse scale
     x /= canvas.width - padding*2;
     y /= canvas.height - padding*2;
-    x *= maxX;
-    y *= maxY;
+    x *= (x - minX) / (maxX - minX);
+    y *= (y - minY) / (maxY - minY);
 
     return {x: x, y: y};
 }
