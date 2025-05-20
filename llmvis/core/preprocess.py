c77d456f2e296ee761b0de1fac5807575913fc31
Alexander Poulson <alexanderipoulson@gmail.com>
2025-05-20 19:53:20 +0100

    chore: add some more banned words

--- llmvis/core/preprocess.py
+++ llmvis/core/preprocess.py
@@ -2,21 +2,27 @@
 Helper functions for the preprocessing of data for visualizations
 """
 
-BANNED_WORDS = [
-    'is',
-    'and',
-    'or',
-    'a',
-    'an',
-    'the',
-    'for',
-    'of',
-    'its',
-    'it\'s',
-    'are',
-    'be',
-    'to'
-]
+BANNED_WORDS = set(
+    [
+        "is",
+        "and",
+        "or",
+        "a",
+        "an",
+        "the",
+        "for",
+        "of",
+        "its",
+        "it's",
+        "are",
+        "be",
+        "to",
+        "in",
+        "has",
+        "it",
+    ]
+)
+
 
 def should_include(word: str) -> bool:
     """
@@ -28,7 +34,7 @@ def should_include(word: str) -> bool:
     Args:
         word (str): The word that should be checked for if it
             is useful.
-    
+
     Returns:
         A `bool` that is `true` if this word is of interest and
         `false` if it is not.
